bcc61a0e19318aa77cb19365c3acdbcdc313d0fc
rmedcraft <rwtmedcraft@gmail.com>
2025-01-07 19:19:24 -0500

    you can create new messages on the client side, TODO: scrollbar when too many messages to fit on screen

--- src/components/InputMessage.tsx
+++ src/components/InputMessage.tsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import { FaPlusCircle } from "react-icons/fa";
 
-export default function InputMessage() {
+export default function InputMessage(props: any) {
+    const setMessages = props.sethook;
+
+    const [message, setMessage] = useState("");
+
+    function addMessage(e: any) {
+        if (e.key === "Enter") {
+            e.preventDefault();
+            setMessages((messages: any[]) => {
+                return messages.concat([{
+                    image: "/src/assets/default-pfp.jpg",
+                    user: "You",
+                    date: "1/7/2024",
+                    message: message
+                }]);
+            });
+            e.target.value = "";
+        }
+    }
+
     return (
-        <div className="relative flex flex-col items-start ml-4">
+        <div className="relative flex flex-col items-start ml-4 shadow-lg">
             <span className="absolute inset-y-0 left-2 flex items-center">
                 <FaPlusCircle className="w-7 h-7 text-gray-600 dark:text-gray-100 hover:text-blue-600 dark:hover:text-blue-200 cursor-pointer transition-all" />
             </span>
-            <input className="h-14 w-[calc(100%-2rem)] pl-11 border-none rounded-xl bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200 focus:outline-none" placeholder="Say something..."></input>
+            <input onKeyDown={addMessage} onChange={(e) => { setMessage(e.target.value); }} className="h-14 w-[calc(100%-2rem)] pl-11 border-none rounded-xl bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200 focus:outline-none" placeholder="Say something..."></input>
         </div>
     );
 }
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,16 +1,25 @@
+import { useState } from "react";
 import InputMessage from "./InputMessage";
 
 export default function MainPage() {
+    const [messages, setMessages] = useState([
+        {
+            image: "/src/assets/default-pfp.jpg",
+            user: "rowan",
+            date: "1/7/2024",
+            message: "WAOW!! THIS IS AN EXAMPLE MESSAGE"
+        }
+    ]);
+
     return (
         <div className="fixed top-14 left-80
                         w-[calc(100vw-20rem)] h-[calc(100vh-3.5rem)]
                         bg-gray-300 dark:bg-gray-600
                         transition-all">
             <ul className="flex flex-col justify-end h-[calc(100%-4rem)] flex-grow overflow-y-auto">
-                <Message image="/src/assets/default-pfp.jpg" user="rowan" date="1/7/2024" message="wAOW!!! THIS IS AN EXAMPLE MESSAGE!!!!!" />
-                <Message image="/src/assets/default-pfp.jpg" user="rowan" date="1/7/2024" message="THIS IS A SECOND MESSAGE!!!!" />
+                {messages.map((message) => <Message image={message.image} user={message.user} date={message.date} message={message.message} />)}
             </ul>
-            <InputMessage />
+            <InputMessage sethook={setMessages} />
         </div>
     );
 }
